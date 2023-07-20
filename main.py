77d3e16ef1914167f4848e84f24a409b206b0b36
Shamil Makhmudov <sh.mahmudov@b5g.ru>
2023-07-20 12:45:17 +0300

    Short version

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import json
 
 
 def get_disk_info():
-    result = [
-        {key.replace('_Disk__', ''): value for key, value in disk.__dict__.items()}
-        for disk in DiskInfo().get_disk_list()
-    ]
-    result_json = json.dumps(
-        obj=result,
-        indent=4
-    )
-
     with open('result.json', "w", encoding="utf-8") as fw:
-        fw.write(result_json)
-
+        fw.write(
+            json.dumps(
+                obj=[
+                    {key.replace('_Disk__', ''): value for key, value in disk.__dict__.items()}
+                    for disk in DiskInfo().get_disk_list()
+                ],
+                indent=4
+            )
+        )
 
 
 if __name__ == "__main__":
